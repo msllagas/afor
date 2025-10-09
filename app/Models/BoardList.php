9fdd1418a47626cbbd3071188b2c10292a6bba00
msllagas <mndyllagas@gmail.com>
2025-10-09 21:09:17 +0800

    feat: add board and card relationship to BoardList model

--- app/Models/BoardList.php
+++ app/Models/BoardList.php
@@ -6,12 +6,29 @@ use Database\Factories\BoardListFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Collection;
 
+/**
+ * @property-read Board $board
+ * @property-read Collection<int, Card> $cards
+ */
 class BoardList extends Model
 {
     /** @use HasFactory<BoardListFactory> */
     use HasUuids, HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function board(): BelongsTo
+    {
+        return $this->belongsTo(Board::class);
+    }
+
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class)->orderBy('order');
+    }
 }
