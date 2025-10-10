59067c16442fbd9c6b72627eac963e2e299e953e
msllagas <mndyllagas@gmail.com>
2025-10-10 22:44:52 +0800

    feat: add boardList relationship to Board model

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -3,13 +3,25 @@
 namespace App\Models;
 
 use Database\Factories\BoardFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property-read Collection<int, BoardList> $boardLists
+ */
 class Board extends Model
 {
     /** @use HasFactory<BoardFactory> */
     use HasUuids, HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public function boardLists(): HasMany
+    {
+        return $this->hasMany(BoardList::class);
+    }
 }
