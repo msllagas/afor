1c0434368bbb454303e4871aa3366f9c132032ef
msllagas <mndyllagas@gmail.com>
2025-10-10 23:02:04 +0800

    feat: create CardController with initial reorder method implemented

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BoardList;
+use App\Models\Card;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Card $card)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Card $card)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Card $card)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Card $card)
+    {
+        //
+    }
+
+
+    public function reorder(Request $request, BoardList $boardList): RedirectResponse
+    {
+        $cards = $request->input('cards', []);
+
+        foreach ($cards as $cardData) {
+            Card::query()->where('id', $cardData['id'])
+                ->where('board_list_id', $boardList->id)
+                ->update(['order' => $cardData['order']]);
+        }
+        return back();
+    }
+}
