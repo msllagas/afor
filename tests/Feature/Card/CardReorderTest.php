6b7fd6db9f1c59b434ae0b83a1c18554c5e99037
msllagas <mndyllagas@gmail.com>
2025-10-12 21:30:59 +0800

    test: add test case for reordering cards on the same board list

---
+++ tests/Feature/Card/CardReorderTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\BoardList;
+use App\Models\Card;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\patch;
+
+uses(RefreshDatabase::class);
+
+test('users can reorder cards in board list', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $boardList = BoardList::factory()->create();
+    $card1 = Card::factory()->create(['board_list_id' => $boardList->id, 'order' => 1]);
+    $card2 = Card::factory()->create(['board_list_id' => $boardList->id, 'order' => 2]);
+    $card3 = Card::factory()->create(['board_list_id' => $boardList->id, 'order' => 3]);
+
+    $payload = [
+        'cards' => [
+            ['id' => $card1->id, 'order' => 2],
+            ['id' => $card2->id, 'order' => 3],
+            ['id' => $card3->id, 'order' => 1],
+        ],
+    ];
+
+    $response = patch(route('board-lists.cards.reorder', [
+        'board_list' => $boardList,
+    ]), $payload);
+
+    assertDatabaseHas('cards', [
+        'id' => $card1->id,
+        'order' => 2,
+    ]);
+
+    assertDatabaseHas('cards', [
+        'id' => $card2->id,
+        'order' => 3,
+    ]);
+
+    assertDatabaseHas('cards', [
+        'id' => $card3->id,
+        'order' => 1,
+    ]);
+});
+
