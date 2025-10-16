05bf6a9a7b9b7bdbadea48fa54e43d62fc84f744
msllagas <mndyllagas@gmail.com>
2025-10-16 23:07:52 +0800

    feat: scaffold form to edit a card and implement update on card name and description

--- resources/js/pages/boards/Show.vue
+++ resources/js/pages/boards/Show.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-import {computed, nextTick, ref, useTemplateRef} from "vue"
+import {computed, nextTick, onMounted, ref, useTemplateRef} from "vue"
 import {Form, Head, router} from "@inertiajs/vue3"
 import draggable from "vuedraggable"
 import {Ellipsis, Plus} from "lucide-vue-next"
 import {Button} from "@/components/ui/button"
 import {Card, CardContent, CardHeader} from "@/components/ui/card"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
 import {Input} from "@/components/ui/input"
-import cards from "@/routes/board-lists/cards"
+import cardRoutes from "@/routes/board-lists/cards"
+import boardRoutes from "@/routes/boards"
 import CardController from "@/actions/App/Http/Controllers/CardController"
 
 interface Card {
@@ -31,9 +39,13 @@ interface Board {
 
 const props = defineProps<{
     board: Board;
+    card?: Card;
 }>();
 
 const drag = ref(false)
+const isDialogOpen = ref(false)
+const isFetching = ref(false)
+
 const dragOptions = computed(() => ({
     animation: 200,
     group: {
@@ -100,7 +112,7 @@ function handleCardMove(boardListId: string) {
         card.order = index;
     });
 
-    router.patch(cards.reorder(boardListId).url, {
+    router.patch(cardRoutes.reorder(boardListId).url, {
         cards: boardList.cards.map(c => ({
             id: c.id,
             order: c.order,
@@ -113,7 +125,7 @@ function handleCardMoveToBoardList(boardListId: string, added: any) {
 
     const card = added.element as Card
 
-    router.patch(cards.update({
+    router.patch(cardRoutes.update({
         board_list: card.board_list_id,
         card: card.id,
     }).url, {
@@ -122,13 +134,73 @@ function handleCardMoveToBoardList(boardListId: string, added: any) {
     })
 }
 
+function onCardClick(boardListId: string, card: Card) {
+    isFetching.value = true
+    isDialogOpen.value = true
+
+    const url = cardRoutes.edit({
+        board_list: boardListId,
+        card: card.id,
+    }).url
+
+    window.history.pushState({}, '', url)
+
+    router.visit(url, {
+        only: ['card'],
+        preserveScroll: true,
+        preserveState: true,
+        replace: true,
+        onSuccess: () => {
+            isFetching.value = false
+        },
+    })
+}
+
+function onDialogClose(value: boolean) {
+    if (!value) {
+        window.history.replaceState({}, '', boardRoutes.show(props.board.id).url)
+
+        router.visit(boardRoutes.show(props.board.id).url, {
+            only: ['board', 'boardLists'],
+            preserveScroll: true,
+            preserveState: true,
+        })
+    }
+}
+
+function autoResize(event: Event) {
+    const target = event.target as HTMLTextAreaElement | null
+    if (!target) return
+
+    target.style.height = 'auto'
+    target.style.height = `${target.scrollHeight}px`
+}
+
+function onSubmit(boardListId: string, card: Card, event: Event) {
+    const target = event.target
+    if (!(target instanceof HTMLInputElement || target instanceof HTMLTextAreaElement)) return
+
+    const name = target.name
+    const value = target.value
+
+    router.patch(cardRoutes.update({board_list: boardListId, card: card.id}).url, {
+        [name]: value,
+    })
+}
+
+onMounted(() => {
+    if (props.card) {
+        isDialogOpen.value = true
+    }
+});
+
 </script>
 
 <template>
-    <Head :title="board.name"/>
+    <Head :title="board?.name ?? card.name"/>
     <div
         class="relative bg-gradient-to-r from-pink-500 via-fuchsia-500 to-rose-400 h-screen overflow-y-auto select-none">
-        <ol v-if="board?.board_lists.length > 0"
+        <ol v-if="board?.board_lists?.length > 0"
             class="h-full p-2 flex overflow-hidden absolute pb-32 gap-2">
             <template v-for="(boardList, index) in board.board_lists" :key="boardList.id">
                 <li class="h-full whitespace-nowrap block shrink-0 self-start">
@@ -166,7 +238,7 @@ function handleCardMoveToBoardList(boardListId: string, added: any) {
                                     <li>
                                         <div class="bg-[#242528] text-white p-2 mb-2 rounded-lg shadow"
                                              :class="{ 'cursor-pointer': !drag }"
-                                             @click="() => console.log(boardList.cards)"
+                                             @click="onCardClick(boardList.id, element)"
                                              :key="element.id"
                                         >
                                             <span class="text-sm">{{ element.name }}</span>
@@ -210,6 +282,61 @@ function handleCardMoveToBoardList(boardListId: string, added: any) {
             </template>
         </ol>
     </div>
+    <Dialog v-model:open="isDialogOpen" @update:open="(value:boolean) => onDialogClose(value)">
+        <DialogContent v-if="isFetching"
+                       class="[&>button:first-of-type]:hidden ">
+            <DialogTitle hidden>Loading</DialogTitle>
+            <DialogDescription hidden>
+                Loading...
+            </DialogDescription>
+        </DialogContent>
+        <DialogContent class="sm:max-w-[600px] max-h-[90dvh] grid-rows-[auto_minmax(0,1fr)_auto]" v-else-if="!isFetching && card">
+            <DialogHeader>
+                <DialogTitle></DialogTitle>
+                <DialogDescription>
+                </DialogDescription>
+            </DialogHeader>
+            <div class="overflow-y-auto">
+                <Form
+                    class="space-y-6"
+                >
+                    <div class="grid gap-4 py-4 overflow-y-auto">
+                        <div>
+                            <h2 class="sr-only">{{ card.name }}</h2>
+                            <div>
+                                <h2 class="sr-only">{{ card.name }}</h2>
+                                <Textarea
+                                    id="name"
+                                    name="name"
+                                    :value="card.name"
+                                    class="w-full text-2xl font-semibold leading-tight resize-none overflow-hidden border-none focus:ring-0 focus:outline-none p-0"
+                                    rows="1"
+                                    @input="autoResize($event)"
+                                    @focus="autoResize($event)"
+                                    @blur="onSubmit(card.board_list_id, card, $event)"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="space-y-4 py-2">
+                        <h3 class="text-sm font-medium">Description</h3>
+                        <Textarea
+                            id="description"
+                            name="description"
+                            :value="card.description"
+                            class="w-full text-sm resize-none border border-gray-300 rounded-md p-2 focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+                            rows="1"
+                            @input="autoResize($event)"
+                            @focus="autoResize($event)"
+                            @blur="onSubmit(card.board_list_id, card, $event)"
+                        />
+
+                    </div>
+                </Form>
+
+            </div>
+        </DialogContent>
+    </Dialog>
 </template>
 
 <style scoped>
