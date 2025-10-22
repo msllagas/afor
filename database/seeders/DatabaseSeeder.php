15600f2e83439b78ae8a26d0a58c81fee5892475
msllagas <mndyllagas@gmail.com>
2025-10-22 22:27:21 +0800

    feat: add factory to create workspace for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Workspace;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->withoutTwoFactor()->create([
+        $user = User::factory()->withoutTwoFactor()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Workspace::factory()->forUser($user)->create();
     }
 }
