db6007e299432fc2f02d6fd59ca030ef2e4d4d15
msllagas <mndyllagas@gmail.com>
2025-10-26 13:02:04 +0800

    feat: define many-to-many relationship to User and Workspace models

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ namespace App\Models;
  use Illuminate\Contracts\Auth\MustVerifyEmail;
  use Illuminate\Database\Eloquent\Concerns\HasUuids;
  use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+ use Illuminate\Database\Eloquent\Relations\HasMany;
+ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
@@ -47,5 +49,18 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function ownedWorkspaces(): HasMany
+    {
+        return $this->hasMany(Workspace::class);
+    }
+
+    // Workspaces this user is a member of
+    public function sharedWorkspaces(): BelongsToMany
+    {
+        return $this->belongsToMany(Workspace::class, 'workspace_user')
+            ->as('sharedWorkspaces')
+            ->withTimestamps();
     }
 }
--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Workspace extends Model
@@ -14,9 +15,17 @@ class Workspace extends Model
     /** @use HasFactory<WorkspaceFactory> */
     use HasFactory, HasUuids;
 
+    // Owner of the workspace
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    // Members of the workspace
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'workspace_user')
+            ->withTimestamps();
     }
 
     public function boards(): HasMany
