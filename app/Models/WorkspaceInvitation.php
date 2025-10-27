f33ee0c679c48b3a0966aab6d38ea07b22b6b2d8
msllagas <mndyllagas@gmail.com>
2025-10-27 21:16:31 +0800

    feat: create model with relationship to workspace and user

---
+++ app/Models/WorkspaceInvitation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class WorkspaceInvitation extends Model
+{
+    use HasUuids;
+
+    protected $table = 'workspace_invitations';
+    protected $guarded = ['id'];
+
+    public function workspace(): BelongsTo
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function inviter(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'invited_by');
+    }
+}
