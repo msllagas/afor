c630dc9b630357f9786331fbba3dc53b217e9bfa
msllagas <mndyllagas@gmail.com>
2025-10-29 22:50:41 +0800

    test: add new test cases for creation of workspace upon registering of new users

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use function Pest\Laravel\assertDatabaseHas;
+
 test('registration screen can be rendered', function () {
     $response = $this->get(route('register'));
 
@@ -16,4 +18,20 @@ test('new users can register', function () {
 
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
+});
+
+test('registering a user automatically creates a default workspace', function () {
+    $response = $this->post(route('register.store'), [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $this->assertAuthenticated();
+
+    assertDatabaseHas('workspaces', [
+        'name' => 'Test User Workspace',
+        'user_id' => auth()->id(),
+    ]);
 });
