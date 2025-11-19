1017c4bdd02c3d47d476bbaa0d63592036263e6b
msllagas <mndyllagas@gmail.com>
2025-11-19 20:52:23 +0800

    feat: create minimal tiptap editor component for editing card description

---
+++ resources/js/components/Tiptap.vue
@@ -0,0 +1,131 @@
+<script lang="ts" setup>
+import { useEditor, EditorContent, Content } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import { List, Bold, Italic, Underline, Heading1, Heading2, Heading3, Heading4 } from 'lucide-vue-next'
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuGroup
+} from "@/components/ui/dropdown-menu";
+import { computed } from "vue";
+
+const model = defineModel();
+const emit = defineEmits(['update:modelValue', 'blur']);
+
+const headings = [1, 2, 3, 4] as const
+const headingIcons = {
+    1: Heading1,
+    2: Heading2,
+    3: Heading3,
+    4: Heading4,
+} as const
+const headingClasses = {
+    1: 'text-3xl font-bold',
+    2: 'text-2xl font-semibold',
+    3: 'text-xl font-semibold',
+    4: 'text-lg font-medium',
+} as const
+
+StarterKit.configure({
+    heading: {
+        levels: [...headings]
+    },
+})
+
+const editor = useEditor({
+    content: model.value as Content,
+    extensions: [StarterKit],
+    onUpdate: ({ editor }) => {
+        emit('update:modelValue', editor.getHTML())
+    },
+    onBlur: ({ editor }) => {
+        emit('blur', editor.getHTML())
+    }
+})
+
+
+const activeHeading = computed(() => {
+    for (const level of headings) {
+        if (editor && editor.value?.isActive('heading', { level })) return level
+    }
+    return null
+})
+</script>
+<template>
+    <div v-if="editor">
+        <div class="flex h-12 items-center gap-2 p-2">
+            <DropdownMenu>
+                <DropdownMenuTrigger as-child>
+                    <Button
+                        class="rounded-2xl"
+                        size="icon"
+                        variant="ghost"
+                    >
+                        <component
+                            :is="headingIcons[activeHeading ?? 1]"
+                            :class="{ 'tiptap-active': activeHeading !== null }"
+                        />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start" class="w-56" side="bottom">
+                    <DropdownMenuGroup>
+                        <DropdownMenuItem
+                            v-for="level in headings"
+                            :key="level"
+                            :class="{ 'bg-[#ffffff33]': editor.isActive('heading', { level }) }"
+                            @click="editor.chain().focus().toggleHeading({ level }).run()"
+                        >
+                            <span
+                                :class="[
+                                    headingClasses[level],
+                                    { 'tiptap-active': editor.isActive('heading', { level })}
+                                    ]"
+                            >
+                                Heading {{ level }}
+                            </span>
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <Button
+                class="rounded-2xl"
+                size="icon"
+                variant="ghost"
+                @click="editor.chain().focus().toggleBulletList().run()"
+            >
+                <List :class="{ 'tiptap-active': editor.isActive('bulletList') }"/>
+            </Button>
+            <Separator orientation="vertical"/>
+            <div>
+                <Button
+                    class="rounded-2xl"
+                    size="icon"
+                    variant="ghost"
+                    @click="editor.chain().focus().toggleBold().run()"
+                >
+                    <Bold :class="{ 'tiptap-active': editor.isActive('bold') }"/>
+                </Button>
+                <Button class="rounded-2xl" size="icon" variant="ghost"
+                        @click="editor.chain().focus().toggleItalic().run()">
+                    <Italic :class="{ 'tiptap-active': editor.isActive('italic') }"/>
+                </Button>
+                <Button class="rounded-2xl text-sm" size="icon" variant="ghost"
+                        @click="editor.chain().focus().toggleUnderline().run()">
+                    <Underline :class="{ 'tiptap-active': editor.isActive('underline') }"/>
+                </Button>
+            </div>
+        </div>
+        <div class="p-4">
+            <editor-content :editor="editor"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
--- resources/js/components/board/board-list/card/CardDialog.vue
+++ resources/js/components/board/board-list/card/CardDialog.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { Form, router } from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3'
 import { Textarea } from '@/components/ui/textarea'
 import {
     Dialog,
@@ -10,6 +10,7 @@ import {
 } from '@/components/ui/dialog'
 import cardRoutes from '@/routes/board-lists/cards'
 import type { Card } from '@/types'
+import Tiptap from "@/components/Tiptap.vue";
 
 defineProps<{
     selectedCard?: Card
@@ -37,12 +38,21 @@ function onSubmit(boardListId: string, cardId: string, event: Event) {
     })
 }
 
+function onSubmitDescription(boardListId: string, cardId: string, value: string) {
+    router.patch(cardRoutes.update({
+        board_list: boardListId,
+        card: cardId
+    }).url, {
+        description: value,
+    })
+}
+
 function autoResize(event: Event) {
     const target = event.target as HTMLTextAreaElement | null
     if (!target) return
 
     target.style.height = 'auto'
-    target.style.height = `${target.scrollHeight}px`
+    target.style.height = `${ target.scrollHeight }px`
 }
 </script>
 
@@ -59,38 +69,39 @@ function autoResize(event: Event) {
             <div v-if="isFetching" class="[&>button:first-of-type]:hidden">
             </div>
             <div v-else-if="!isFetching && selectedCard" class="overflow-y-auto">
-                <Form
+                <div
                     class="space-y-6"
                 >
                     <div class="grid gap-4 py-4 overflow-y-auto">
-                            <div>
-                                <h2 class="sr-only">{{ selectedCard.name }}</h2>
-                                <Textarea
-                                    id="name"
-                                    :model-value="selectedCard.name"
-                                    class="w-full text-2xl font-semibold leading-tight resize-none overflow-hidden border-none focus:ring-0 focus:outline-none p-0"
-                                    name="name"
-                                    rows="1"
-                                    un-styled
-                                    @keydown.enter.prevent="$event.target.blur()"
-                                    @blur="onSubmit(selectedCard.board_list_id, selectedCard.id, $event)"
-                                    @focus="autoResize"
-                                    @input="autoResize"
-                                />
-                            </div>
+                        <div>
+                            <h2 class="sr-only">{{ selectedCard.name }}</h2>
+                            <Textarea
+                                id="name"
+                                :model-value="selectedCard.name"
+                                class="w-full text-2xl font-semibold leading-tight resize-none overflow-hidden border-none focus:ring-0 focus:outline-none p-0"
+                                name="name"
+                                rows="1"
+                                un-styled
+                                @blur="onSubmit(selectedCard.board_list_id, selectedCard.id, $event)"
+                                @focus="autoResize"
+                                @input="autoResize"
+                                @keydown.enter.prevent="$event.target.blur()"
+                            />
+                        </div>
                     </div>
                     <div class="space-y-4 py-2">
                         <h3 class="text-sm font-medium">Description</h3>
-                        <Textarea
-                            id="description"
-                            :model-value="selectedCard.description"
-                            class="w-full text-sm resize-none border border-gray-300 rounded-md p-2  focus:ring-1 "
-                            name="description"
-                            rows="1"
-                            un-styled
-                        />
+                        <div class="w-full rounded-lg border focus-within:border-pink-100 transition-colors">
+                            <Tiptap
+                                :model-value="selectedCard.description"
+                                name="description"
+                                @blur="onSubmitDescription(selectedCard.board_list_id, selectedCard.id, $event)"
+                                @update:model-value="console.log($event)"
+                            />
+                        </div>
+
                     </div>
-                </Form>
+                </div>
             </div>
         </DialogContent>
     </Dialog>
