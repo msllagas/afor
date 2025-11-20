9496b60e3dd4b75d51b4a633a273caba9c243d10
msllagas <mndyllagas@gmail.com>
2025-11-20 22:01:03 +0800

    refactor: add if condition to show only the shared section of workspaces if any

--- resources/js/pages/boards/Index.vue
+++ resources/js/pages/boards/Index.vue
@@ -120,83 +120,85 @@ onMounted(() => {
                         </div>
                     </template>
                 </div>
-                <div class="my-5 space-y-1">
-                    <h3 class="text-gray-300 font-semibold">SHARED WITH YOU</h3>
-                    <h4 class="text-sm text-gray-500">Workspaces where you're a member</h4>
-                </div>
-                <div class="">
-                    <template v-for="sharedWorkspace in props.sharedWorkspaces" :key="sharedWorkspace.id">
-                        <div class="pb-10">
-                            <div class="mb-4">
-                                <h3 class="font-semibold">{{ sharedWorkspace.name }}</h3>
-                            </div>
-                            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-                                <template v-for="board in sharedWorkspace.boards" :key="board.id">
-                                    <Link :href="boardsRoutes.show(board.id).url">
-                                        <Card
-                                            :class="cn('w-full overflow-hidden rounded-2xl shadow-lg pt-0 gap-2 pb-2', $attrs.class ?? '')"
-                                        >
-                                            <CardContent
-                                                class="relative h-24 overflow-hidden bg-gradient-to-r from-pink-500 via-fuchsia-500 to-rose-400 flex items-end p-4 group"
+                <section v-if="props.sharedWorkspaces.length">
+                    <div class="my-5 space-y-1">
+                        <h3 class="text-gray-300 font-semibold">SHARED WITH YOU</h3>
+                        <h4 class="text-sm text-gray-500">Workspaces where you're a member</h4>
+                    </div>
+                    <div class="">
+                        <template v-for="sharedWorkspace in props.sharedWorkspaces" :key="sharedWorkspace.id">
+                            <div class="pb-10">
+                                <div class="mb-4">
+                                    <h3 class="font-semibold">{{ sharedWorkspace.name }}</h3>
+                                </div>
+                                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                                    <template v-for="board in sharedWorkspace.boards" :key="board.id">
+                                        <Link :href="boardsRoutes.show(board.id).url">
+                                            <Card
+                                                :class="cn('w-full overflow-hidden rounded-2xl shadow-lg pt-0 gap-2 pb-2', $attrs.class ?? '')"
                                             >
-                                                <!-- overlay -->
-                                                <div
-                                                    class="absolute inset-0 bg-black/0 group-hover:bg-black/20 transition-colors duration-300"
-                                                ></div>
-                                            </CardContent>
-                                            <CardFooter class="m-0.5 px-6">
+                                                <CardContent
+                                                    class="relative h-24 overflow-hidden bg-gradient-to-r from-pink-500 via-fuchsia-500 to-rose-400 flex items-end p-4 group"
+                                                >
+                                                    <!-- overlay -->
+                                                    <div
+                                                        class="absolute inset-0 bg-black/0 group-hover:bg-black/20 transition-colors duration-300"
+                                                    ></div>
+                                                </CardContent>
+                                                <CardFooter class="m-0.5 px-6">
                                                 <span class="relative text-sm text-white font-semibold drop-shadow-md">
                                                     {{ board.name }}
                                                 </span>
-                                            </CardFooter>
-                                        </Card>
-                                    </Link>
-                                </template>
-                                <Popover>
-                                    <PopoverTrigger as-child>
-                                        <Card
-                                            :class="cn('w-full overflow-hidden bg-sidebar hover:bg-sidebar-accent rounded-2xl shadow-lg pt-0 gap-2 pb-2', $attrs.class ?? '')"
-                                        >
-                                            <CardContent
-                                                class="relative h-32 overflow-hidden flex items-center justify-center"
+                                                </CardFooter>
+                                            </Card>
+                                        </Link>
+                                    </template>
+                                    <Popover>
+                                        <PopoverTrigger as-child>
+                                            <Card
+                                                :class="cn('w-full overflow-hidden bg-sidebar hover:bg-sidebar-accent rounded-2xl shadow-lg pt-0 gap-2 pb-2', $attrs.class ?? '')"
                                             >
+                                                <CardContent
+                                                    class="relative h-32 overflow-hidden flex items-center justify-center"
+                                                >
                             <span class="relative text-sm text-white font-semibold drop-shadow-md">
                                     Create new board
                             </span>
-                                            </CardContent>
-                                        </Card>
-                                    </PopoverTrigger>
-                                    <PopoverContent class="rounded-xl bg-sidebar">
-                                        <Form
-                                            v-slot="{ errors, processing }"
-                                            class="space-y-6"
-                                            v-bind="BoardController.store.form({workspace: sharedWorkspace.id})"
-                                        >
-                                            <div class="grid gap-2">
-                                                <Label class="test-sm" for="name">Board Name</Label>
-                                                <Input
-                                                    id="name"
-                                                    class="mt-1 block w-full"
-                                                    name="name"
-                                                    required
-                                                />
-                                                <InputError :message="errors.name" class="mt-2"/>
-                                            </div>
-                                            <div class="flex items-center gap-4">
-                                                <Button
-                                                    :disabled="processing"
-                                                    data-test="add-board-button"
-                                                >Create
-                                                </Button
-                                                >
-                                            </div>
-                                        </Form>
-                                    </PopoverContent>
-                                </Popover>
+                                                </CardContent>
+                                            </Card>
+                                        </PopoverTrigger>
+                                        <PopoverContent class="rounded-xl bg-sidebar">
+                                            <Form
+                                                v-slot="{ errors, processing }"
+                                                class="space-y-6"
+                                                v-bind="BoardController.store.form({workspace: sharedWorkspace.id})"
+                                            >
+                                                <div class="grid gap-2">
+                                                    <Label class="test-sm" for="name">Board Name</Label>
+                                                    <Input
+                                                        id="name"
+                                                        class="mt-1 block w-full"
+                                                        name="name"
+                                                        required
+                                                    />
+                                                    <InputError :message="errors.name" class="mt-2"/>
+                                                </div>
+                                                <div class="flex items-center gap-4">
+                                                    <Button
+                                                        :disabled="processing"
+                                                        data-test="add-board-button"
+                                                    >Create
+                                                    </Button
+                                                    >
+                                                </div>
+                                            </Form>
+                                        </PopoverContent>
+                                    </Popover>
+                                </div>
                             </div>
-                        </div>
-                    </template>
-                </div>
+                        </template>
+                    </div>
+                </section>
             </div>
         </div>
     </AppLayout>
