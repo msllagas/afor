a910aa0a7a9619bb31c5f4265237f2459fca12c7
msllagas <mndyllagas@gmail.com>
2025-12-25 17:45:37 +0800

    style: change text tag and reformat code with Prettier

--- resources/js/pages/workspace-invitations/Invite.vue
+++ resources/js/pages/workspace-invitations/Invite.vue
@@ -1,52 +1,47 @@
 <script lang="ts" setup>
-import type {Invitation} from '@/types';
-import {Button} from "@/components/ui/button";
-import workspaceInvitationsRoutes from "@/routes/workspace-invitations";
-import {router} from "@inertiajs/vue3";
-
+import { Button } from '@/components/ui/button';
+import workspaceInvitationsRoutes from '@/routes/workspace-invitations';
+import type { Invitation } from '@/types';
+import { router } from '@inertiajs/vue3';
 
 const props = defineProps<{
-    invitation: Invitation,
-    savedToken?: boolean
+    invitation: Invitation;
+    savedToken?: boolean;
 }>();
 
 function acceptInvitation() {
-    router.post(workspaceInvitationsRoutes.accept({
-        workspace: props.invitation.workspace.id,
-        token: props.invitation.token
-    }))
+    router.post(
+        workspaceInvitationsRoutes.accept({
+            workspace: props.invitation.workspace.id,
+            token: props.invitation.token,
+        }),
+    );
 }
 
 function handleLogin() {
-    console.log('handle login here')
+    console.log('handle login here');
 }
 function handleRegister() {
-    console.log('handle register here')
+    console.log('handle register here');
 }
 </script>
 
 <template>
-    <div>
-        <h1><strong>{{ invitation.inviter.name }}</strong> is inviting you to <strong>{{
-                invitation.workspace.name
-            }}</strong></h1>
-
-        <div class="flex items-center gap-4">
-            <Button v-if="$page.props.auth.user" @click="acceptInvitation">
+    <div class="space-y-6 py-10 text-center">
+        <div class="text-gray-900 md:text-xl lg:text-2xl dark:text-white">
+            <strong>{{ invitation.inviter.name }}</strong> is inviting you to
+            <strong>{{ invitation.workspace.name }}</strong>
+        </div>
+        <div>
+            <Button v-if="$page.props.auth.user" class="cursor-pointer" @click="acceptInvitation">
                 Accept Invitation
             </Button>
             <template v-else>
-                <Button class="cursor-pointer" variant="ghost" @click="handleLogin">
-                    Login
-                </Button>
-                <Button class="cursor-pointer" variant="outline" @click="handleRegister">
-                    Register
-                </Button>
+                <Button class="cursor-pointer" variant="ghost" @click="handleLogin"> Login </Button>
+                <Button class="cursor-pointer" variant="outline" @click="handleRegister"> Register </Button>
             </template>
         </div>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
