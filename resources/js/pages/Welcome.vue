cceb4fdb1bef20950a706f01a51b7a8be1c33fe6
msllagas <mndyllagas@gmail.com>
2025-12-27 10:33:03 +0800

    style: reformat code with prettier

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,22 +1,20 @@
 <script lang="ts" setup>
+import { Button } from '@/components/ui/button';
 import { dashboard, login, register } from '@/routes';
 import { Head, Link } from '@inertiajs/vue3';
-import { Button } from "@/components/ui/button";
 </script>
 
 <template>
     <Head title="Welcome">
-        <link href="https://rsms.me/" rel="preconnect"/>
-        <link href="https://rsms.me/inter/inter.css" rel="stylesheet"/>
+        <link href="https://rsms.me/" rel="preconnect" />
+        <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
     </Head>
-    <div
-        class=""
-    >
+    <div class="">
         <header
-            class="sticky h-16 top-0 z-50 w-full border-b bg-background/95 backdrop-blur dark:border-neutral-800/60"
+            class="sticky top-0 z-50 h-16 w-full border-b bg-background/95 backdrop-blur dark:border-neutral-800/60"
         >
             <nav class="mx-auto h-full max-w-6xl px-4 sm:px-6 lg:px-8">
-                <div class="flex items-center justify-between h-full">
+                <div class="flex h-full items-center justify-between">
                     <div>
                         <h1 class="text-xl font-semibold text-pink-500">Afor</h1>
                     </div>
@@ -43,20 +41,19 @@ import { Button } from "@/components/ui/button";
                             </Link>
                         </template>
                     </div>
-
                 </div>
             </nav>
         </header>
         <main>
             <section class="">
                 <div class="mx-auto max-w-6xl px-4 pt-12">
-                    <div class="max-w-2xl mx-auto text-center">
+                    <div class="mx-auto max-w-2xl text-center">
                         <div>
                             <div>
-                                <h1 class="text-4xl lg:text-6xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                <h1 class="text-4xl font-bold tracking-tight text-gray-900 lg:text-6xl dark:text-white">
                                     The Simple Way to Stay Organized
                                 </h1>
-                                <p class="lg:text-xl text-gray-500 dark:text-gray-400 mt-2 lg:mt-4">
+                                <p class="mt-2 text-gray-500 lg:mt-4 lg:text-xl dark:text-gray-400">
                                     Plan better, work smarter, and accomplish more with Afor.
                                 </p>
                             </div>
