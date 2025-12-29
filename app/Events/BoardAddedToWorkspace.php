abbf7ff2eea4fa0bba979d0274e4809776e3c00c
msllagas <mndyllagas@gmail.com>
2025-12-29 21:23:54 +0800

    feat: crate new broadcasting event for creating board in workspace

---
+++ app/Events/BoardAddedToWorkspace.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Board;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class BoardAddedToWorkspace implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public Board $board,
+        public string $workspaceId
+    ) {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('workspace.'.$this->workspaceId),
+        ];
+    }
+}
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('workspace.{workspaceId}', function (User $user, $workspaceId) {
+    return ! is_null($user->workspaces->firstWhere('id', $workspaceId));
 });
